5fb296268c6849d9aa82024aa232c9002333e669
Jonathan Fielding <jonathan@macbook.home>
2011-05-06 13:26:09 +0100

    added support for touch devices

--- jquery.hoverpanels.js
+++ jquery.hoverpanels.js
@@ -3,8 +3,9 @@
   $.fn.hoverpanel = function( options ) {  
 
     var settings = {
-	  'default_content'  : '.default',
-	  'hover_content'    : '.hover',
+	  'default_content'  	: '.default',
+	  'hover_content'    	: '.hover',
+	  'fade_level'    		: '0.5'
     };
 
     return this.each(function() {        
@@ -15,22 +16,43 @@
       }
 
 		$(this).find(settings.hover_content).hide();
-
-		$(this).mouseenter(function(){
-			$(this).stop(true);
-			$(this).siblings().stop(true)
-			$(this).siblings().fadeTo('','0.50');
-			$(this).fadeTo('','1');
-			$(this).find(settings.default_content).hide();
-			$(this).find(settings.hover_content).show();
-		});
 		
-		$(this).mouseleave(function(){
-			$(this).fadeTo('','1');
-			$(this).siblings().fadeTo('','1');
-			$(this).find(settings.default_content).show();
-			$(this).find(settings.hover_content).hide();
-		});
+		if(isTouchDevice()){
+			$(this).click(function(){
+				$(this).stop(true);
+				$(this).siblings().stop(true)
+				$(this).siblings().fadeTo('',settings.fade_level);
+				$(this).fadeTo('','1');
+				$(this).find(settings.default_content).hide();
+				$(this).find(settings.hover_content).show();
+			});
+		}
+		else {
+			$(this).mouseenter(function(){
+				$(this).stop(true);
+				$(this).siblings().stop(true)
+				$(this).siblings().fadeTo('',settings.fade_level);
+				$(this).fadeTo('','1');
+				$(this).find(settings.default_content).hide();
+				$(this).find(settings.hover_content).show();
+			});
+			
+			$(this).mouseleave(function(){
+				$(this).fadeTo('','1');
+				$(this).siblings().fadeTo('','1');
+				$(this).find(settings.default_content).show();
+				$(this).find(settings.hover_content).hide();
+			});
+		}
+		
+		function isTouchDevice() {
+		    try {
+		        document.createEvent("TouchEvent");
+		        return true;
+		    } catch (e) {
+		        return false;
+		    }
+		}
     });
 
   };
